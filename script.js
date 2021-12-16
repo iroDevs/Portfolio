d82eb65dbfb3452b5322deeedaec13e1cc7ab07b
iroDevs <ironegrolol@gamil.com>
2021-12-16 14:09:45 -0300

    version 2.0

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector('#btn');
 let myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'))
 
+
 btn.addEventListener('click', ()=>{ 
   myModal.show();
 
